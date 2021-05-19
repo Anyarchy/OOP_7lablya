444b2d4afb6a39b8b6f7c888a130d8b9ba17b208
heroofgotham <annamikhnenko9@gmail.com>
2021-05-20 02:26:53 +0300

    Add project files.

---
+++ Equation.cs
@@ -0,0 +1,29 @@
+﻿using static System.Math;
+using System;
+
+namespace OOP_7lablya
+{
+    public class Equation
+    {
+        private double a, b, c;
+        public double Result { get; set; }
+
+        public Equation(double x1, double x2, double x3)
+        {
+            a = x1;
+            b = x2;
+            c = x3;
+        }
+
+
+        public void Function()
+        {
+            if (a * 2 + b / c == 0)
+                throw new DivideByZeroException("Division by zero");
+            if (b <= 1)
+                throw new ArgumentException("Wrong arguments");
+            Result = ((8 * Log10(b - 1) - c) / (2 * a + b / c));
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace OOP_7lablya
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Equation[] array = new Equation[3];
+            array[0] = new Equation(4, 2, 3);
+            array[1] = new Equation(2, -8, 2);
+            array[2] = new Equation(3, 1, 4);
+            for (int i = 0; i < 3; i++)
+            {
+                try
+                {
+                    array[i].Function();
+                    Console.WriteLine($"Result of equation[{i}]: {array[i].Result}");
+                }
+
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine($"Result of equation[{i}]: Devision by zero ");
+                }
+
+                catch (ArgumentException)
+                {
+                    Console.WriteLine($"Result of equation[{i}]: Wrong arguments ");
+                }
+            }
+
+        }
+    }
+}
